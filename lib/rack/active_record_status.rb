7b37c42f7a11737fb47a35e945a60f1c61541e96
Leonid Sobolievskyi <leonid@sobolievskyi.name>
2015-01-05 22:58:14 -0300

    Move to repository

---
+++ lib/rack/active_record_status.rb
@@ -0,0 +1,35 @@
+module Rack
+  class ActiveRecordStatus
+    def initialize(app, options={})
+      @app = app
+      # Support legacy arguments (0.4.1 and below)
+      options = {:path => options} if options.is_a?(String)
+
+      @path = options[:path] || '/app_status'
+      @response = options[:response] || "OK\n"
+    end
+
+    def call(env)
+      if @path == env['PATH_INFO']
+        get_status
+      else
+        @app.call(env)
+      end
+    end
+
+    def get_status
+      begin
+        # Check that the application is connected to the database
+        ActiveRecord::Base.silence do
+          ActiveRecord::Base.connection.select_all('select 1')
+        end
+        # Success
+        [200, {'Content-Type' => 'text/plain'}, [@response]]
+      rescue
+        body = ['ERROR', "#{$!.class}: #{$!.message}", "Backtrace:"] + $!.backtrace
+        body *= "\n"
+        [500, {'Content-Type' => 'text/plain'}, [body]]
+      end
+    end
+  end
+end
---
+++ rack-active_record_status.gemspec
@@ -0,0 +1,25 @@
+Gem::Specification.new do |s|
+  s.name = 'rack-active_record_status'
+  s.version = '0.6.1'
+
+  s.summary = "Rack middelware to check ActiveRecord's connection"
+  s.description = "A server health check for active_record."
+
+  s.authors  = ["Aaron Suggs"]
+  s.email    = 'aaron@ktheory.com'
+  s.homepage = 'https://github.com/ktheory/rack-active_record_status'
+
+  s.rdoc_options = ["--charset=UTF-8"]
+  s.extra_rdoc_files = %w[README.rdoc]
+
+  s.add_dependency 'rack'
+  s.add_dependency 'activerecord'
+
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'rake'
+  s.add_development_dependency 'shoulda'
+  s.add_development_dependency 'rack-test'
+
+  s.files = `git ls-files`.split("\n")
+  s.test_files = `git ls-files -- {spec,tests}/*`.split("\n")
+end
